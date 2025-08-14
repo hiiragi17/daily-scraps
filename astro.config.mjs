8315452eb6290c42fd03ea8426f5f0ee68321841
mami.hirono <mami.hirono@boxyz.com>
2025-08-15 00:38:45 +0900

    add:GitHub Actionsの設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
-// @ts-check
-
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://hiiragi17.github.io',
+  base: '/daily-scraps',
+  integrations: [
+    mdx(),
+    sitemap()
+  ],
+  output: 'static',
+  build: {
+    inlineStylesheets: 'auto'
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name].[hash][extname]'
+        }
+      }
+    }
+  }
 });
